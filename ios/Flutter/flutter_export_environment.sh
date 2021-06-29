dce3e982e8dba7e2ab1b6aaa586af4cf372fb363
zenkouto <koutozen@gmail.com>
2021-06-29 14:15:40 +0530

    test build

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\development\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Admin\AndroidStudioProjects\RestaurantAppUIKit"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/home/zen/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/zen/AndroidStudioProjects/projectMed"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=5.0.0"
 export "FLUTTER_BUILD_NUMBER=50"
 export "DART_OBFUSCATION=false"
